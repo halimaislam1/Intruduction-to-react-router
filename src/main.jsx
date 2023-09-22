3ffd3e93403d8068784769ee3da5aea0f340603e
halimaislam1 <halimaislam6181@gmail.com>
2023-09-22 17:27:09 +0600

    added Home component and apply it main jsx inside element

--- src/main.jsx
+++ src/main.jsx
@@ -3,29 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello From React Router</div>,
+//   },
+//   {
+//     path: '/about',
+//     element: <div>I am in the about page</div>
+//   },
+//   {
+//     path: '/contact',
+//     element: <div>Call me right now!!</div>
+//   }
+// ]);
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello From React Router</div>,
-  },
-  {
-    path: '/about',
-    element: <div>I am in the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>Call me right now!!</div>
+    path: '/',
+    element: <Home></Home>
   }
-]);
-
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <App></App> */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
